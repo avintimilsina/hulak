aa22be6a74e5d8f307ec919dd47550f4826ace2f
avintimilsina <avin.timilsina.16@gmail.com>
2023-05-04 23:57:11 +0545

    chore(web): :card_file_box: set users data in firestore

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,10 +1,49 @@
+import theme from "@/config/theme";
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { doc, setDoc } from "firebase/firestore";
+import type { AppProps } from "next/app";
+import { Router } from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "../../firebase";
 
-const App = ({ Component, pageProps }: AppProps) => (
-	<ChakraProvider>
-		<Component {...pageProps} />
-	</ChakraProvider>
-);
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
+const App = ({ Component, pageProps }: AppProps) => {
+	const [currentUser] = useAuthState(auth);
+	useEffect(() => {
+		const setUser = async () => {
+			if (currentUser) {
+				await setDoc(
+					doc(db, "users", currentUser?.uid),
+					{
+						email: currentUser?.email,
+						photoURL: currentUser?.photoURL,
+						displayName: currentUser?.displayName,
+						uid: currentUser?.uid,
+						phoneNumber: currentUser?.phoneNumber,
+						providerData: currentUser?.providerData,
+						emailVerified: currentUser?.emailVerified,
+						username: currentUser?.email?.split("@")[0],
+						createdAt: currentUser?.metadata?.creationTime,
+					},
+					{ merge: true }
+				);
+			}
+		};
+
+		setUser();
+	}, [currentUser]);
+
+	return (
+		<ChakraProvider theme={theme}>
+			<Component {...pageProps} />
+		</ChakraProvider>
+	);
+};
 export default App;
