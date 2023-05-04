3c278a52d2bad623bc89be1e22711ae940a18b6c
avintimilsina <avin.timilsina.16@gmail.com>
2023-05-04 23:29:16 +0545

    feat(web): :sparkles: added register page

---
+++ apps/web/src/pages/auth/register.tsx
@@ -0,0 +1,10 @@
+import RegisterForm from "@/components/auth/RegisterForm";
+import { Grid } from "@chakra-ui/react";
+
+const Register = () => (
+	<Grid placeItems="center" h="100vh">
+		<RegisterForm />
+	</Grid>
+);
+
+export default Register;
