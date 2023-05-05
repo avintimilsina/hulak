a61ce91eba20d96bb4892e349d99c9729dd24030
avintimilsina <avin.timilsina.16@gmail.com>
2023-05-05 23:42:02 +0545

    feat(web): :sparkles: added modal button

---
+++ apps/web/src/components/ui/ModalButton.tsx
@@ -0,0 +1,66 @@
+import {
+	Button,
+	Modal,
+	ModalOverlay,
+	ModalContent,
+	ModalHeader,
+	ModalCloseButton,
+	ModalBody,
+	ModalFooter,
+	ButtonProps,
+	useDisclosure,
+} from "@chakra-ui/react";
+import { forwardRef, useImperativeHandle, type ReactNode } from "react";
+
+interface ModalButtonProps extends ButtonProps {
+	buttonText: string;
+	modalHeader?: ReactNode;
+	modalFooter?: ReactNode;
+}
+
+const ModalButton = forwardRef(
+	(
+		{
+			buttonText,
+			children,
+			modalHeader,
+			modalFooter,
+			...rest
+		}: ModalButtonProps,
+		ref
+	) => {
+		const { isOpen, onOpen, onClose } = useDisclosure();
+
+		useImperativeHandle(ref, () => ({
+			closeModal() {
+				onClose();
+			},
+		}));
+
+		return (
+			<>
+				<Button onClick={onOpen} {...rest}>
+					{buttonText}
+				</Button>
+				<Modal size="lg" isOpen={isOpen} onClose={onClose}>
+					<ModalOverlay />
+					<ModalContent>
+						{modalHeader && <ModalHeader>{modalHeader}</ModalHeader>}
+						<ModalCloseButton />
+						<ModalBody>{children}</ModalBody>
+						{modalFooter && <ModalFooter>{modalFooter}</ModalFooter>}
+					</ModalContent>
+				</Modal>
+			</>
+		);
+	}
+);
+
+ModalButton.displayName = "ModalButton";
+
+ModalButton.defaultProps = {
+	modalHeader: null,
+	modalFooter: null,
+};
+
+export default ModalButton;
