fbd08daa79e3a4bb683b02b8bb2d20413265a430
Rohit Sah <sahrohit9586@gmail.com>
2023-05-12 02:51:21 +0200

    refactor(web): :lipstick: minor ui changes in quote form

--- apps/web/src/components/pages/quote/SendTo.tsx
+++ apps/web/src/components/pages/quote/SendTo.tsx
@@ -5,63 +5,68 @@ import {
 	HStack,
 	Heading,
 	Input,
-	Stack,
+	VStack,
 } from "@chakra-ui/react";
 import { Field } from "formik";
 
 const SendTo = () => (
-	<Stack spacing="4" direction="row">
-		<Heading size="md">Receiver Details</Heading>
-		<Field name="destinationPincode">
-			{({ field, form }: any) => (
-				<FormControl
-					isInvalid={
-						form.errors.destinationPincode && form.touched.destinationPincode
-					}
-				>
-					<HStack justifyContent="space-between">
-						<FormLabel>Pincode</FormLabel>
-						<FormErrorMessage>
-							{form.errors.destinationPincode}
-						</FormErrorMessage>
-					</HStack>
-					<Input {...field} type="text" maxLength={255} />
-				</FormControl>
-			)}
-		</Field>
-		<Field name="destinationCity">
-			{({ field, form }: any) => (
-				<FormControl
-					isInvalid={
-						form.errors.destinationCity && form.touched.destinationCity
-					}
-				>
-					<HStack justifyContent="space-between">
-						<FormLabel>City</FormLabel>
-						<FormErrorMessage>{form.errors.destinationCity}</FormErrorMessage>
-					</HStack>
-					<Input {...field} type="text" maxLength={255} />
-				</FormControl>
-			)}
-		</Field>
-		<Field name="destinationDistrict">
-			{({ field, form }: any) => (
-				<FormControl
-					isInvalid={
-						form.errors.destinationDistrict && form.touched.destinationDistrict
-					}
-				>
-					<HStack justifyContent="space-between">
-						<FormLabel>District</FormLabel>
-						<FormErrorMessage>
-							{form.errors.destinationDistrict}
-						</FormErrorMessage>
-					</HStack>
-					<Input {...field} type="text" maxLength={255} />
-				</FormControl>
-			)}
-		</Field>
-	</Stack>
+	<VStack spacing="4" w="full">
+		<Heading size="md" w="full">
+			Reciever Details
+		</Heading>
+		<HStack w="full">
+			<Field name="destinationPincode">
+				{({ field, form }: any) => (
+					<FormControl
+						isInvalid={
+							form.errors.destinationPincode && form.touched.destinationPincode
+						}
+					>
+						<HStack justifyContent="space-between">
+							<FormLabel>Pincode</FormLabel>
+							<FormErrorMessage>
+								{form.errors.destinationPincode}
+							</FormErrorMessage>
+						</HStack>
+						<Input {...field} type="text" maxLength={255} />
+					</FormControl>
+				)}
+			</Field>
+			<Field name="destinationCity">
+				{({ field, form }: any) => (
+					<FormControl
+						isInvalid={
+							form.errors.destinationCity && form.touched.destinationCity
+						}
+					>
+						<HStack justifyContent="space-between">
+							<FormLabel>City</FormLabel>
+							<FormErrorMessage>{form.errors.destinationCity}</FormErrorMessage>
+						</HStack>
+						<Input {...field} type="text" maxLength={255} />
+					</FormControl>
+				)}
+			</Field>
+			<Field name="destinationDistrict">
+				{({ field, form }: any) => (
+					<FormControl
+						isInvalid={
+							form.errors.destinationDistrict &&
+							form.touched.destinationDistrict
+						}
+					>
+						<HStack justifyContent="space-between">
+							<FormLabel>District</FormLabel>
+							<FormErrorMessage>
+								{form.errors.destinationDistrict}
+							</FormErrorMessage>
+						</HStack>
+						<Input {...field} type="text" maxLength={255} />
+					</FormControl>
+				)}
+			</Field>
+		</HStack>
+	</VStack>
 );
 
 export default SendTo;
--- apps/web/src/pages/quote.tsx
+++ apps/web/src/pages/quote.tsx
@@ -3,8 +3,9 @@ import calculatePostage from "@/components/helpers/calculatePostage";
 import WhatForm from "@/components/pages/create/WhatForm";
 import SendFrom from "@/components/pages/quote/SendFrom";
 import SendTo from "@/components/pages/quote/SendTo";
-import { Box, Button, Heading, VStack } from "@chakra-ui/react";
+import { Button, Heading, Stack, Text, VStack } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
+import { useState } from "react";
 import * as Yup from "yup";
 
 const defaultValues = {
@@ -35,44 +36,63 @@ const QuoteFormSchema = Yup.object({
 	packageHeight: Yup.number().required("Required"),
 	packageValue: Yup.number().required("Required"),
 });
-const QuotePage = () => (
-	<Formik
-		initialValues={defaultValues}
-		validationSchema={QuoteFormSchema}
-		onSubmit={async (values) => {
-			const postagePrice = await calculatePostage(
-				values.packageHeight,
-				values.packageWeight,
-				values.packageLength,
-				values.packageWidth,
-				values.sourceCity,
-				values.destinationCity
-			);
-			console.log("PostagePrice", postagePrice);
-		}}
-	>
-		{() => (
-			<Form>
-				<Box
-					mx="auto"
-					maxW="4xl"
-					py="10"
-					px={{ base: "6", md: "8" }}
-					minH="400px"
-				>
-					<Heading>Quote</Heading>
-					<VStack gap="4">
-						<SendFrom />
-						<SendTo />
-					</VStack>
-					<Heading>Pacakge Information</Heading>
-					<WhatForm />
+const QuotePage = () => {
+	const [cost, setCost] = useState(0);
+	const [distance, setDistance] = useState(0);
+	const [volume, setVolume] = useState(0);
 
-					<Button type="submit">Submit</Button>
-				</Box>
-			</Form>
-		)}
-	</Formik>
-);
+	return (
+		<Formik
+			initialValues={defaultValues}
+			validationSchema={QuoteFormSchema}
+			onSubmit={async (values) => {
+				const {
+					postageCost,
+					distance: calculatedDistance,
+					volume: calculatedVolume,
+				} = await calculatePostage(
+					values.packageHeight,
+					values.packageWeight,
+					values.packageLength,
+					values.packageWidth,
+					values.sourceCity,
+					values.destinationCity
+				);
+				setCost(postageCost);
+				setDistance(calculatedDistance);
+				setVolume(calculatedVolume);
+			}}
+		>
+			{() => (
+				<Form>
+					<Heading textAlign="center">Quote</Heading>
+					<Stack
+						direction={["column", "column", "row"]}
+						p="10"
+						px={{ base: "6", md: "16" }}
+						gap={8}
+					>
+						<VStack mx="auto" maxW="4xl" gap={4} px={{ base: 4, lg: 24 }}>
+							<VStack gap="4">
+								<SendFrom />
+								<SendTo />
+							</VStack>
+							<Heading size="md" w="full">
+								Package Information
+							</Heading>
+							<WhatForm />
+						</VStack>
+						<VStack flexGrow={1}>
+							<Text>Distance: {distance}</Text>
+							<Text>Postage Volume: {volume}</Text>
+							<Text>Postage Cost: {cost}</Text>
+							<Button type="submit">Submit</Button>
+						</VStack>
+					</Stack>
+				</Form>
+			)}
+		</Formik>
+	);
+};
 
 export default QuotePage;
