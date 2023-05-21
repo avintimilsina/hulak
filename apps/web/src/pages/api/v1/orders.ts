ec59ed6756395015da285ce50f3c8aa88fbf54a5
Rohit Sah <sahrohit9586@gmail.com>
2023-05-21 20:11:32 +0200

    feat(both): :sparkles: added order by id api

--- apps/web/src/pages/api/v1/orders.ts
+++ apps/web/src/pages/api/v1/orders.ts
@@ -5,18 +5,71 @@ import firebaseAdminInit from "../../../components/helpers/firebaseAdminInit";
 const { db } = firebaseAdminInit();
 
 type Data = {
-	orders: any;
+	orders?: any;
+	order?: any;
+};
+
+type Errors = {
+	message: string;
+	error: string;
 };
 
 export default async function handler(
 	req: NextApiRequest,
-	res: NextApiResponse<Data>
+	res: NextApiResponse<Data | Errors>
 ) {
-	const data: any = [];
-	const querySnapshot = await db.collectionGroup("orders").get();
-	querySnapshot.forEach((doc: any) => {
-		data.push(doc.data());
-	});
+	try {
+		const keyCheck = await db
+			.collection("keys")
+			.doc(req.headers.key as string)
+			.get();
 
-	res.status(200).json({ orders: data });
+		if (!keyCheck.exists) {
+			res.status(401).json({
+				message: "Unauthorized",
+				error: "Invalid API Key",
+			});
+			return;
+		}
+
+		if (!keyCheck.data()?.isActive) {
+			res.status(401).json({
+				message: "Unauthorized",
+				error: "Expired API Key",
+			});
+			return;
+		}
+
+		const { userId } = keyCheck.data() as any;
+		if (req.method === "GET") {
+			const data: any = [];
+			const querySnapshot = await db
+				.collection("orders")
+				.where("userId", "==", userId)
+				.get();
+			querySnapshot.forEach((doc: any) => {
+				data.push(doc.data());
+			});
+
+			res.status(200).json({ orders: data });
+		} else {
+			const data = await db
+				.collection("orders")
+				.doc(req.body.id as string)
+				.get();
+
+			if (data.data()?.userId !== userId) {
+				res.status(401).json({
+					message: "Unauthorized",
+					error: "Invalid User",
+				});
+				return;
+			}
+			res.status(200).json({ order: data.data() });
+		}
+	} catch (error: any) {
+		res
+			.status(500)
+			.json({ message: "Internal Server Error", error: error.message });
+	}
 }
