d1024f7971e8d06ebbc2f1ba8d6baf66330cca41
avintimilsina <avin.timilsina.16@gmail.com>
2023-05-23 17:24:43 +0545

    fix(web): :art: added buttons to access different pages in the homepage

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import PageLoadingSpinner from "@/components/shared/PageLoadingSpinner";
 import Result from "@/components/shared/Result";
-import { Button, Grid, Text } from "@chakra-ui/react";
+import { Button, Text, VStack } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { useAuthState, useSignOut } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase";
@@ -24,11 +24,15 @@ const Homepage = () => {
 	}
 
 	return (
-		<Grid placeItems="left" h="100vh">
+		<VStack h="100vh" alignItems="flex-start">
 			{currentUser ? (
-				<Button onClick={signOut}>Sign Out</Button>
+				<Button w="3xs" my="4" onClick={signOut}>
+					Sign Out
+				</Button>
 			) : (
 				<Button
+					w="3xs"
+					my="4"
 					onClick={() => {
 						router.push("/auth/login");
 					}}
@@ -36,10 +40,55 @@ const Homepage = () => {
 					Login
 				</Button>
 			)}
+			<VStack gap="2" w="full" alignItems="flex-start">
+				<Button
+					onClick={() => {
+						router.push("/auth/register");
+					}}
+				>
+					Register
+				</Button>
+				<Button
+					onClick={() => {
+						router.push("/quote");
+					}}
+				>
+					Quote
+				</Button>
+				<Button
+					onClick={() => {
+						router.push("/create-order");
+					}}
+				>
+					Create Order
+				</Button>
+				<Button
+					onClick={() => {
+						router.push("/tracking");
+					}}
+				>
+					Tracking
+				</Button>
+				<Button
+					onClick={() => {
+						router.push("/support");
+					}}
+				>
+					Support
+				</Button>
+				<Button
+					onClick={() => {
+						router.push("/payment");
+					}}
+				>
+					Payment
+				</Button>
+			</VStack>
+
 			<Text as="pre" w="2xl">
 				{JSON.stringify(currentUser, null, 2)}
 			</Text>
-		</Grid>
+		</VStack>
 	);
 };
 
