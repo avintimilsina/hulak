5166440a69b8c2b5f67266c88615cbac07906ec3
avintimilsina <avin.timilsina.16@gmail.com>
2023-05-29 13:00:13 +0545

    feat(web): :art: added user profile skeleton in sidebar

--- apps/web/src/components/shared/dashboard/sidebar/UserProfile.tsx
+++ apps/web/src/components/shared/dashboard/sidebar/UserProfile.tsx
@@ -3,6 +3,7 @@ import {
 	Button,
 	Flex,
 	HStack,
+	Skeleton,
 	Text,
 	VStack,
 	useToast,
@@ -13,48 +14,78 @@ import { auth } from "../../../../../firebase";
 const UserProfile = () => {
 	const toast = useToast();
 	const [currentUser, userLoading, userError] = useAuthState(auth);
-	const [signOut, loading, error] = useSignOut(auth);
-
-	if (loading || userLoading) return <p>Loading...</p>;
+	const [signOut, , error] = useSignOut(auth);
 
 	if (error || userError) return <p>Error: {error?.message}</p>;
 
 	return (
 		<VStack gap={4}>
-			<HStack spacing="4" px="2" w="full">
-				<Avatar
-					name="Avin Timilsina"
-					src={
-						currentUser?.photoURL ??
-						`https://api.dicebear.com/6.x/micah/svg?size=256&seed=${currentUser?.displayName}`
-					}
-				/>
-				<Flex direction="column">
-					<Text fontWeight="medium">{currentUser?.displayName}</Text>
-					<Text fontSize="sm" lineHeight="shorter">
-						{currentUser?.email}
-					</Text>
-				</Flex>
-			</HStack>
-			<Button
-				w="full"
-				colorScheme="red"
-				onClick={async () => {
-					const success = await signOut();
-					if (success) {
-						toast({
-							title: "Logged out",
-							description: "You have been logged out.",
-							status: "success",
-							isClosable: true,
-						});
-					}
-				}}
-			>
-				Logout
-			</Button>
+			{userLoading ? (
+				<UserProfileSkeleton />
+			) : (
+				<>
+					<HStack spacing="4" px="2" w="full">
+						<Avatar
+							name="Avin Timilsina"
+							src={
+								currentUser?.photoURL ??
+								`https://api.dicebear.com/6.x/micah/svg?size=256&seed=${currentUser?.displayName}`
+							}
+						/>
+						<Flex direction="column">
+							<Text fontWeight="medium">{currentUser?.displayName}</Text>
+							<Text fontSize="sm" lineHeight="shorter">
+								{currentUser?.email}
+							</Text>
+						</Flex>
+					</HStack>
+					<Button
+						w="full"
+						colorScheme="red"
+						onClick={async () => {
+							const success = await signOut();
+							if (success) {
+								toast({
+									title: "Logged out",
+									description: "You have been logged out.",
+									status: "success",
+									isClosable: true,
+								});
+							}
+						}}
+					>
+						Logout
+					</Button>
+				</>
+			)}
 		</VStack>
 	);
 };
 
 export default UserProfile;
+
+const UserProfileSkeleton = () => (
+	<>
+		<HStack spacing="4" px="2" w="full">
+			<Skeleton>
+				<Avatar
+					name="Avin Timilsina"
+					src="https://api.dicebear.com/6.x/micah/svg?size=256&seed=avintimilsina"
+				/>
+			</Skeleton>
+			<Flex direction="column">
+				<Skeleton>
+					<Text fontWeight="medium">Avin Timilsina</Text>
+				</Skeleton>
+				<Skeleton>
+					<Text fontSize="sm" lineHeight="shorter">
+						avin.timilsina.16@gmail.com
+					</Text>
+				</Skeleton>
+			</Flex>
+		</HStack>
+		<Skeleton w="full">
+			<Button>Logout</Button>
+		</Skeleton>
+	</>
+);
