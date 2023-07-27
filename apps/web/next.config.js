1adbd15105dd677e197476d7342fa05ed39fb92a
avintimilsina <avin.timilsina.16@gmail.com>
2023-07-28 00:45:24 +0545

    feat(web): :wheelchair: added pwa

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,9 +1,24 @@
 /** @type {import('next').NextConfig} */
+
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+	dest: "public",
+	register: true,
+	skipWaiting: true,
+	runtimeCaching,
+	disable: process.env.NODE_ENV === "development",
+});
+
 const nextConfig = {
+  productionBrowserSourceMaps: true,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com'],
-  },
+    domains: ['images.unsplash.com', 'lh3.googleusercontent.com'],
+  }
 }
 
-module.exports = nextConfig
+module.exports = module.exports =withPWA(nextConfig);
